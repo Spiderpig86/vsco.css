6f93779fe9a390e0d1aac6fc113e1b44e2e2198a
Stan <slim679975@gmail.com>
2018-04-01 02:33:59 -0400

    feat(base.scss): Started working on some base styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('compile', function() {
+    gulp.src('./src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe($.concat('app.css'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['compile'], function() {
+    return gulp.src(['./dist/app.css'])
+        .pipe(minify())
+        .pipe($.size())
+        .pipe($.concat('app.min.css'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Watch task
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/**/*.scss', ['compile']);
+});
